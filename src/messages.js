70843bb23febecf38f7f007dba41623f625b41f7
MahmoudElsayed <mahmoud.elsayed.eg.1993@gmail.com>
2024-02-23 20:20:42 +0200

    re

--- src/messages.js
+++ src/messages.js
@@ -1,8 +1,10 @@
 export default {
   en: {
-    title: 'eng'
+    name: 'Mahmoud Elsayed',
+    summary:'Passionate Web Developer with a proven track record of leading the development of innovative web applications. Skilled in PHP, Laravel, Vue 3, and a variety of web technologies, with expertise in creating dynamic and multilingual websites. Led the development of a versatile content management system for an artisanal olive and olive oil producer, enabling seamless management of bilingual content and facilitating the expansion of their online presence. Orchestrated the development of feature-rich applications, including menu and order apps, café table management systems, and a comprehensive retail management application, demonstrating proficiency in Laravel and Vue 3. Strong ability to work independently and manage multiple projects simultaneously, with a commitment to continuous learning and improvement. Previously excelled as an Android Developer, delivering high-quality apps and collaborating closely with clients to exceed expectations. Effective communicator fluent in English and Arabic.'
   },
   ar: {
-    title: 'عرب'
+    name: 'محمود السيد',
+    summary:'مطور ويب متحمس بسجل حافل من قيادة تطوير تطبيقات ويب مبتكرة. ماهر في PHP، Laravel، Vue 3، ومجموعة متنوعة من تقنيات الويب، مع خبرة في إنشاء مواقع الويب الديناميكية ومتعددة اللغات. قدت تطوير نظام إدارة محتوى متعدد الاستخدامات لمنتج ومنتجع زيت الزيتون الحرفي، مما يمكن من إدارة سلسة للمحتوى ثنائي اللغة وتيسير توسيع وجودهم على الإنترنت. قمت بتنظيم تطوير تطبيقات غنية بالميزات، بما في ذلك تطبيقات القوائم والطلبات، وأنظمة إدارة طاولات المقاهي، وتطبيق إدارة التجزئة الشامل، مما يدل على اجادتي في Laravel و Vue 3. لدي قدرة قوية على العمل بشكل مستقل وإدارة عدة مشاريع في وقت واحد، مع التزام بالتعلم المستمر والتحسين. سبق لي أن أبرز كمطور أندرويد، وقدمت تطبيقات عالية الجودة وتعاون عن كثب مع العملاء لتجاوز توقعاتهم. متواصل فعّال باللغتين الإنجليزية والعربية.'
   }
 }
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -3,6 +3,7 @@
     <button @click="$i18n.locale = 'en'" class="bg-white">en</button>
     <button @click="$i18n.locale = 'ar'">ar</button>
   </div>
-  <h1>{{ $t('title') }}</h1>
+  <h1>{{ $t('name') }}</h1>
+  <p :dir="$i18n.locale == 'ar' ? 'rtl':'ltr'">{{ $t('summary') }}</p>
 </template>
 <script setup></script>
