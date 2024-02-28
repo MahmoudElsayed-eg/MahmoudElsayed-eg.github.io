a5fbf508b4b3f2e07d186a5f6bc933aff64ca838
Mahmoud Elsayed <mahmoud.elsayed.eg.1993@gmail.com>
2024-02-28 18:46:25 +0200

    some major changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main :dir="$i18n.locale == 'ar' ? 'rtl' : 'ltr'">
+    <h1>{{ $t('hello') }}</h1>
+    <h2>{{ $t('job_title') }}</h2>
+    <p>{{ $t('summary') }}</p>
+    <p>{{ $t('projects') }}</p>
   </main>
 </template>
