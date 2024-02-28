0641c0318c439f2cfa3ef9eb45b6f85207f120b5
Mahmoud Elsayed <mahmoud.elsayed.eg.1993@gmail.com>
2024-02-28 18:46:25 +0200

    some major changes

--- src/messages.js
+++ src/messages.js
@@ -1,10 +1,17 @@
 export default {
   en: {
-    name: 'Mahmoud Elsayed',
-    summary:'Passionate Web Developer with a proven track record of leading the development of innovative web applications. Skilled in PHP, Laravel, Vue 3, and a variety of web technologies, with expertise in creating dynamic and multilingual websites. Led the development of a versatile content management system for an artisanal olive and olive oil producer, enabling seamless management of bilingual content and facilitating the expansion of their online presence. Orchestrated the development of feature-rich applications, including menu and order apps, café table management systems, and a comprehensive retail management application, demonstrating proficiency in Laravel and Vue 3. Strong ability to work independently and manage multiple projects simultaneously, with a commitment to continuous learning and improvement. Previously excelled as an Android Developer, delivering high-quality apps and collaborating closely with clients to exceed expectations. Effective communicator fluent in English and Arabic.'
+    hello: "Marhaba , I'm Mahmoud Elsayed",
+    job_title: 'A Fullstack web developer and Android developer based in Egypt',
+    summary:
+      'Passionate Web & Android Developer with expertise in PHP, Laravel, Vue 3, and Android app development. Led the creation of dynamic, multilingual websites and feature-rich applications.Skilled in managing multiple projects independently and committed to continuous improvement. Fluent in English and Arabic.',
+    projects: 'Projects',
+    
   },
   ar: {
-    name: 'محمود السيد',
-    summary:'مطور ويب متحمس بسجل حافل من قيادة تطوير تطبيقات ويب مبتكرة. ماهر في PHP، Laravel، Vue 3، ومجموعة متنوعة من تقنيات الويب، مع خبرة في إنشاء مواقع الويب الديناميكية ومتعددة اللغات. قدت تطوير نظام إدارة محتوى متعدد الاستخدامات لمنتج ومنتجع زيت الزيتون الحرفي، مما يمكن من إدارة سلسة للمحتوى ثنائي اللغة وتيسير توسيع وجودهم على الإنترنت. قمت بتنظيم تطوير تطبيقات غنية بالميزات، بما في ذلك تطبيقات القوائم والطلبات، وأنظمة إدارة طاولات المقاهي، وتطبيق إدارة التجزئة الشامل، مما يدل على اجادتي في Laravel و Vue 3. لدي قدرة قوية على العمل بشكل مستقل وإدارة عدة مشاريع في وقت واحد، مع التزام بالتعلم المستمر والتحسين. سبق لي أن أبرز كمطور أندرويد، وقدمت تطبيقات عالية الجودة وتعاون عن كثب مع العملاء لتجاوز توقعاتهم. متواصل فعّال باللغتين الإنجليزية والعربية.'
+    hello: 'مرحبا , أنا محمود السيد',
+    job_title: 'مطور ويب Fullstack ومطور أندرويد مقره مصر',
+    summary:
+      'مطور ويب ومطور أندرويد متحمس ذو خبرة في PHP و Laravel و Vue 3 وتطوير تطبيقات Android. قاد إنشاء مواقع ويب ديناميكية متعددة اللغات وتطبيقات غنية بالمميزات. ماهر في إدارة مشاريع متعددة بشكل مستقل وملتزم بالتحسين المستمر. متقن للغة الإنجليزية والعربية.',
+    projects: 'المشاريع',
   }
 }
